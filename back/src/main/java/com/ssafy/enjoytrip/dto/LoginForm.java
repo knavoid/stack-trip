3848d78f931d9ab15020e3b0534c8a0f0185dfec
knavoid <k_shun@naver.com>
2023-05-16 17:13:10 +0900

    #2 fix: 로그인 폼 DTO 추가

---
+++ back/src/main/java/com/ssafy/enjoytrip/dto/LoginForm.java
@@ -0,0 +1,14 @@
+package com.ssafy.enjoytrip.dto;
+
+import lombok.AllArgsConstructor;
+import lombok.Getter;
+
+@Getter
+@AllArgsConstructor
+public class LoginForm {
+
+	private String email;
+
+	private String password;
+
+}
