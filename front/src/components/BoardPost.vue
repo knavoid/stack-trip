6d27f9878ce5c8859afa208491b4ac6c982658cc
knavoid <k_shun@naver.com>
2023-05-22 11:49:12 +0900

    #8 feat: 게시판 목록 및 게시글 작성

--- front/src/components/BoardPost.vue
+++ front/src/components/BoardPost.vue
@@ -1,16 +1,16 @@
 <template>
   <div class="container">
     <h1>게시글 작성</h1>
-    <b-form @submit.prevent="submitForm">
+    <b-form @submit.prevent="onSubmit">
       <b-form-group label="게시글 종류" label-for="type">
         <b-form-select
           id="type"
-          v-model="form.type"
+          v-model="selectedType"
           :options="types"
         ></b-form-select>
       </b-form-group>
-      <b-form-group label="제목" label-for="title">
-        <b-form-input id="title" v-model="form.title"></b-form-input>
+      <b-form-group label="제목" label-for="subject">
+        <b-form-input id="subject" v-model="form.subject"></b-form-input>
       </b-form-group>
       <b-form-group label="내용" label-for="content">
         <b-form-textarea
@@ -19,29 +19,62 @@
           :rows="5"
         ></b-form-textarea>
       </b-form-group>
-      <b-button type="submit" variant="primary">작성 완료</b-button>
+      <b-button class="mb-5" type="submit" variant="primary"
+        >작성 완료</b-button
+      >
     </b-form>
   </div>
 </template>
 
 <script>
+import http from "@/util/http-common.js";
 export default {
   data() {
     return {
       form: {
-        type: "",
-        title: "",
+        userCode: 5,
+        userName: "",
+        subject: "",
         content: "",
+        type: 0,
       },
       types: ["공지사항", "이벤트", "관광지 후기", "자유게시판"],
+      selectedType: "",
     };
+  },
+  methods: {
+    onSubmit(evt) {
+      evt.preventDefault();
+
+      let err = true;
+      if (this.form.subject.length > 0 && this.form.content.length > 0) {
+        err = false;
+      }
+      if (err) {
+        alert("제목과 내용을 입력해주세요.");
+        return;
+      } else {
+        this.form.type = this.types.indexOf(this.selectedType) + 1;
+        console.log(this.form.type);
+        alert("글이 등록되었습니다.");
+        http
+          .post("/post", this.form, {
+            headers: {
+              Authorization: `Bearer ${sessionStorage.getItem("token")}`,
+            },
+          })
+          .then(() => {
+            this.$router.push("/board");
+          });
+      }
+    },
   },
 };
 </script>
 
 <style scoped>
 .container {
-    margin-top: 20px;
-    border: 1px solid #000000;
+  margin-top: 20px;
+  border: 1px solid #000000;
 }
 </style>
