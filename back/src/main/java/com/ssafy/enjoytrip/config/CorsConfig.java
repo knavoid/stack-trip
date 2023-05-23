2d4089a0cb9262a2376722957037515c09f4a611
knavoid <k_shun@naver.com>
2023-05-24 02:24:42 +0900

    build: host 변경

--- back/src/main/java/com/ssafy/enjoytrip/config/CorsConfig.java
+++ back/src/main/java/com/ssafy/enjoytrip/config/CorsConfig.java
@@ -13,7 +13,9 @@ public class CorsConfig implements WebMvcConfigurer {
 
     @Override
     public void addCorsMappings(CorsRegistry registry) {
-        registry.addMapping("/**").allowedOrigins("http://70.12.50.48:3000", "http://70.12.50.47:3000", "http://70.12.50.48:8080").allowedMethods("*")
+        registry.addMapping("/**")
+                .allowedOrigins("http://127.0.0.1:3000", "http://127.0.0.1:8080", "http://70.12.50.48:8080")
+                .allowedMethods("*")
                 .allowCredentials(true);
     }
 
@@ -22,8 +24,7 @@ public class CorsConfig implements WebMvcConfigurer {
         UrlBasedCorsConfigurationSource source = new UrlBasedCorsConfigurationSource();
         CorsConfiguration config = new CorsConfiguration();
         config.setAllowCredentials(true);
-        config.addAllowedOrigin("http://70.12.50.47:3000");
-        config.addAllowedOrigin("http://70.12.50.48:3000");
+        config.addAllowedOrigin("http://127.0.0.1:3000");
         config.addAllowedHeader("*");
         config.addAllowedMethod("*");
         config.addExposedHeader("Authorization"); // CORS로 인해 프론트단에서 인식하지 못하는 Authrization 헤더를 노출
