884b10f51d6caae28b2015544c396c9ab7504c78
knavoid <k_shun@naver.com>
2023-05-24 02:23:36 +0900

    #9 북마크 관리 페이지 기능 연결

--- front/src/main.js
+++ front/src/main.js
@@ -1,19 +1,20 @@
-import Vue from 'vue'
-import App from './App.vue'
-import router from './router'
-import store from './store'
-import BootstrapVue from "bootstrap-vue"
-import "bootstrap/dist/css/bootstrap.min.css"
-import "bootstrap-vue/dist/bootstrap-vue.css"
-import VueGeolocation from "vue-geolocation-api"
+import Vue from "vue";
+import App from "./App.vue";
+import router from "./router";
+import store from "./store";
+import { BootstrapVue, BootstrapVueIcons } from "bootstrap-vue";
+import "bootstrap/dist/css/bootstrap.min.css";
+import "bootstrap-vue/dist/bootstrap-vue.css";
+import VueGeolocation from "vue-geolocation-api";
 
-Vue.use(BootstrapVue)
-Vue.use(VueGeolocation)
+Vue.use(BootstrapVue);
+Vue.use(VueGeolocation);
+Vue.use(BootstrapVueIcons);
 
-Vue.config.productionTip = false
+Vue.config.productionTip = false;
 
 new Vue({
-  router,
-  store,
-  render: h => h(App)
-}).$mount('#app')
+    router,
+    store,
+    render: (h) => h(App),
+}).$mount("#app");
--- front/src/views/BookmarkView.vue
+++ front/src/views/BookmarkView.vue
@@ -1,58 +1,107 @@
 <template>
-    <div>
-      <h1>위시리스트</h1>
-      <hr>
-      <b-card-group class="d-flex flex-wrap">
-        <b-card class=" mb-3" v-for="item in wishlist" :key="item.id" style="width: 300px">
-          <b-card-img :src="item.image" :alt="item.name"></b-card-img>
-          <b-card-body class>
-            <h5 class="card-title">{{ item.name }}</h5>
-            <p class="card-text">{{ item.description }}</p>
-            <b-button variant="danger" @click="removeFromWishlist(item.id)">위시리스트에서 제거</b-button>
-          </b-card-body>
-        </b-card>
-      </b-card-group>
-      
-      <div v-if="wishlist.length === 0">위시리스트가 비어 있습니다.</div>
-    </div>
-  </template>
+  <div>
+    <h1>위시리스트</h1>
+    <hr />
+    <b-card-group class="d-flex flex-wrap">
+      <b-card class="mb-3" v-for="(item, index) in wishList" :key="index">
+        <b-card-img
+          :src="item.image"
+          :alt="item.title"
+          @error="
+            item.image = 'https://eumseongcci.korcham.net/images/no-image01.gif'
+          "
+        ></b-card-img>
+        <b-card-body class>
+          <h5 class="card-title">{{ item.title }}</h5>
+          <p class="card-text">{{ item.address }}</p>
+          <b-icon
+            v-if="item.isBookmarked"
+            icon="suit-heart-fill"
+            variant="danger"
+            scale="1.5"
+            @click="removeBookmark(index, item.attractionId)"
+          ></b-icon>
+          <b-icon
+            v-else
+            icon="suit-heart"
+            variant="danger"
+            scale="1.5"
+            @click="createBookmark(index, item.attractionId)"
+          ></b-icon>
+        </b-card-body>
+      </b-card>
+    </b-card-group>
+
+    <div v-if="wishList.length === 0">위시리스트가 비어 있습니다.</div>
+  </div>
+</template>
   
-  <script>
-  export default {
-    data() {
-      return {
-        wishlist: [
-          {
-            id: 1,
-            name: '상품 1',
-            description: '이 상품은 매우 멋진 상품입니다.',
-            image: 'http://tong.visitkorea.or.kr/cms/resource/21/2657021_image2_1.jpg'
-          },
-          {
-            id: 2,
-            name: '상품 2',
-            description: '이 상품은 훌륭한 성능을 가진 상품입니다.',
-            image: 'http://tong.visitkorea.or.kr/cms/resource/62/219162_image2_1.jpg'
-          },
-          {
-            id: 3,
-            name: '상품 3',
-            description: '이 상품은 놀라운 기능을 제공하는 상품입니다.',
-            image: 'http://tong.visitkorea.or.kr/cms/resource/83/1070183_image2_1.jpg'
-          }
-        ]
-      };
-    },
-    methods: {
-      removeFromWishlist(id) {
-        // 위시리스트에서 특정 상품을 제거하는 메소드
-        // 해당 상품을 wishlist 배열에서 제거합니다.
-        this.wishlist = this.wishlist.filter(item => item.id !== id);
-      }
+<script>
+import http from "@/util/http-common.js";
+export default {
+  data() {
+    return {
+      userCode: null,
+      wishList: [],
+    };
+  },
+  created() {
+    if (sessionStorage.getItem("token") === null) {
+      this.$router.push("/login");
+    } else {
+      this.getWishList();
     }
-  };
-  </script>
+  },
+  methods: {
+    getWishList() {
+      http
+        .get(`/user`, {
+          headers: {
+            Authorization: `Bearer ${sessionStorage.getItem("token")}`,
+          },
+        })
+        .then(({ data }) => {
+          this.userCode = data.userCode;
+          http
+            .get(`/bookmark/${this.userCode}`, {
+              headers: {
+                Authorization: `Bearer ${sessionStorage.getItem("token")}`,
+              },
+            })
+            .then(({ data }) => {
+              data.forEach((element) => {
+                element.isBookmarked = true;
+                this.wishList.push(element);
+              });
+            });
+        });
+    },
+    removeBookmark(index, attractionId) {
+      this.wishList[index].isBookmarked = false;
+      http.delete(
+        `/bookmark?attractionId=${attractionId}&userCode=${this.userCode}`,
+        {
+          headers: {
+            Authorization: `Bearer ${sessionStorage.getItem("token")}`,
+          },
+        }
+      );
+    },
+    createBookmark(index, attractionId) {
+      this.wishList[index].isBookmarked = true;
+      http.post(
+        `/bookmark?attractionId=${attractionId}&userCode=${this.userCode}`,
+        { attractionId: attractionId, userCode: this.userCode },
+        {
+          headers: {
+            Authorization: `Bearer ${sessionStorage.getItem("token")}`,
+          },
+        }
+      );
+    },
+  },
+};
+</script>
   
 <style scoped>
-
 </style>
